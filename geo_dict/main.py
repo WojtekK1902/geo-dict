468ccc08b6251ec400ec539764d1267c37368659
WojtekK1902 <wojciech.korczynski@gmail.com>
2014-12-13 02:34:37 +0100

    Tests

--- geo_dict/main.py
+++ geo_dict/main.py
@@ -9,17 +9,32 @@ def test(text, model_coords):
     print min([(calculate_distance(model_coords[0], model_coords[1], c[0], c[1]), (c[0], c[1])) for c in coords])
 
 
-# text = u'Opis problemu: skrzyżowanie Łukasińskiego i Huculskiej. Duża wyrwa w drodze.'
-# model_coords = (50.022075850566914, 19.926117202377327)
+text = u'Opis problemu: skrzyżowanie Łukasińskiego i Huculskiej. Duża wyrwa w drodze.'
+model_coords = (50.022075, 19.926117)
 
-# test(text, model_coords)
+test(text, model_coords)
 
-# text = u'Między wroclawska a mazowiecka są jakies osiedla.'
-# model_coords = (50.0775898, 19.9225592)
+text = u'Między wroclawska a mazowiecka są jakies osiedla.'
+model_coords = (50.0775898, 19.9225592)
 
-# test(text, model_coords)
+test(text, model_coords)
 
 text = u'Czekałem na Kijowskiej, na skrzyżowaniu obok Biprostalu'
 model_coords = (50.072973, 19.915578)
+
+test(text, model_coords)
+
+text = u'Pomiędzy ulicą Świętego Jana a Jamą Michalika znajduje się Muzeum Czartoryskich'  # NER is needed both in places and streets
+model_coords = (50.064624, 19.939964)
+
+test(text, model_coords)
+
+text = u'Co jest pomiędzy ulicą Szujskiego a AGH?'
+model_coords = (50.064266, 19.927148)
+
+test(text, model_coords)
+
+text = u'Czekam przy Czarnowiejskiej obok AGH'
+model_coords = (50.066275, 19.922667)
 
 test(text, model_coords)
